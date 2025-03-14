6e92f99d91bb5b57b172c653b634394df5dfaebf
Shagun Kaushik <shagun.kaushik@hp.com>
2025-03-14 18:32:40 +0530

    First Commit
    First Commit

---
+++ 1_Hellos.js
@@ -0,0 +1,10 @@
+var Employee_details = {
+    Empname: "John",
+    EmpSection: "Engineering"
+};
+console.log("Employee Name is: ", Employee_details.Empname, "\nEmployee's section is: ", Employee_details.EmpSection);
+var firstName = "Dylan";
+console.log(firstName);
+var names = [];
+names.push("Dylan");
+console.log(names);
---
+++ 1_Hellos.ts
@@ -0,0 +1,22 @@
+let Employee_details = {
+    Empname: "John",
+    EmpSection: "Engineering"
+}
+console.log("Employee Name is: ", Employee_details.Empname,
+    "\nEmployee's section is: ", Employee_details.EmpSection
+)
+
+
+let firstName: string = "Dylan";
+console.log(firstName);
+
+let names: string[] = [];
+names.push("Dylan"); 
+names.push("John");
+names.push("Jane");
+names.push("Doe");
+names.push("Smith");
+names.push("Doe");
+console.log(names);
+
+
